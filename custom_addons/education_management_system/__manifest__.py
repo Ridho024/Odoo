e1ef3369010652d565245b88d95ab190c18e6a7c
ridho777 <ridho.artsys@gmail.com>
2025-02-22 10:34:42 +0700

    feat(manifest): mengubah deskripsi dan keterangan module.

--- custom_addons/education_management_system/__manifest__.py
+++ custom_addons/education_management_system/__manifest__.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "education_management_system",
+    'name': "Education Management System",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Education Management System (Manajemen Sekolah/Kursus)",
 
     'description': """
-Long description of module's purpose
+    Fitur utama:
+        - Manajemen siswa, guru, dan kelas.
+        - Jadwal pelajaran & absensi.
+        - Manajemen ujian & nilai.
+        - Pembayaran biaya pendidikan & faktur otomatis.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    
+    'author': "Muhammad Ridho Ramadhan",
+    
+    'website': "https://github.com/Ridho024",
+    
+    'category': 'Manage',
+    
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    
     'depends': ['base'],
-
-    # always loaded
+    
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+    
     'demo': [
         'demo/demo.xml',
     ],
