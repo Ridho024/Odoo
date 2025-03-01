75e88ed3bc40e911aa9072e9a847eb97e4588067
ridho777 <ridho.artsys@gmail.com>
2025-03-01 07:33:03 +0700

    feat(student_class): menambahkan compute total student dan fix class_schedule error

--- custom_addons/education/models/student_class.py
+++ custom_addons/education/models/student_class.py
@@ -13,10 +13,16 @@ class StudentClass(models.Model):
     department = fields.Many2one('student.department', string='Jurusan', help='Jurusan kelas jika.')
     academic_year = fields.Char(string='Tahun Ajar', required=True, help='Tahun akademik kelas.')
     teacher_id = fields.Many2one('teacher.teacher', string='Wali Kelas',help='Wali kelas.')
-    total_student = fields.Integer(string='Total Siswa', help='Jumlah siswa di kelas', readonly=True)
+    total_student = fields.Integer(string='Total Siswa', help='Jumlah siswa di kelas', readonly=True, compute='_compute_total_student')
     student_ids = fields.One2many('student.student', 'class_id', string='Siswa/Siswi', help='Siswa/siswi kelas.')    
     schedule_ids = fields.One2many('class.schedule', 'class_id', string='Jadwal Pelajaran', help='Jadwal pelajaran kelas.')
     attendance_ids = fields.One2many('class.attendance', 'class_id', string='Absensi Siswa', help='Absensi siswa kelas.')
+    
+    @api.depends('student_ids')
+    def _compute_total_student(self):
+        for rec in self:
+            rec.total_student = len(rec.student_ids)
+    
 
 class ClassSchedule(models.Model):
     _name = 'class.schedule'
@@ -35,8 +41,8 @@ class ClassSchedule(models.Model):
     start_time = fields.Float(string='Jam Mulai', required=True, help='Jam mulai pelajaran.')
     end_time = fields.Float(string='Jam Selesai', required=True, help='Jam selesai pelajaran.')
     duration = fields.Float(string='Durasi', required=True, help='Durasi pelajaran.')
-    classroom = fields.Many2one('classroom.classroom', string='Ruang Kelas', help='Ruang kelas.')
-    class_id = fields.Many2one('education.class', string='Kelas', help='Kelas.')
+    classroom_id = fields.Many2one('classroom.classroom', string='Ruang Kelas', help='Ruang kelas.')
+    class_id = fields.Many2one('student.class', string='Kelas', help='Kelas.')
 
 class StudentAttendance(models.Model):
     _name = 'class.attendance'
