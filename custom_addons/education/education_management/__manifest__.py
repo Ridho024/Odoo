8aeee85e6a9e58f28949fd4d566f6f2db2f41d54
ridho777 <ridho.artsys@gmail.com>
2025-03-11 09:52:38 +0700

    feat(education_schedule): Menambahkan views teacher schedule

--- custom_addons/education/education_management/__manifest__.py
+++ custom_addons/education/education_management/__manifest__.py
@@ -21,6 +21,7 @@
         'views/education_student_views.xml',
         'views/education_student_attendance_views.xml',
         'views/education_teacher_views.xml',
+        'views/education_teacher_schedule_views.xml',
         'views/education_student_menu_views.xml',
     ],
     'demo': [
--- custom_addons/education/education_management/models/education_schedule.py
+++ custom_addons/education/education_management/models/education_schedule.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class EducationSchedule(models.Model):
     _name = 'education.schedule'
     _description = 'Class Schedule'
+    _rec_name = "course_id"     
     
     course_id = fields.Many2one('education.course', string='Course', required=True)
     classroom_id = fields.Many2one('education.classroom', string='Classroom', required=True)
@@ -19,7 +20,7 @@ class EducationSchedule(models.Model):
     start_time = fields.Float(string='Start Time', required=True, help='Start time in 24-hour format (e.g., 9.00 for 9:00 AM)')
     end_time = fields.Float(string='End Time', required=True, help='End time in 24-hour format (e.g., 11.00 for 11:00 AM)')
     duration = fields.Float(string='Duration (hours)', compute='_compute_duration', store=True)
-    note = fields.Text(string='Notes')
+    notes = fields.Text(string='Notes')
     
     @api.depends('start_time', 'end_time')
     def _compute_duration(self):
