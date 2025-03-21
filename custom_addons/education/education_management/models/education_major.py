68129870c23dff42cf229657ec5d31c446c3ed66
ridho777 <ridho.artsys@gmail.com>
2025-03-21 13:13:13 +0700

    feat(education_major): Update teacher_id and teacher_ids relation to res.partner

--- custom_addons/education/education_management/models/education_major.py
+++ custom_addons/education/education_management/models/education_major.py
@@ -6,8 +6,8 @@ class EducationMajor(models.Model):
     
     name = fields.Char(string='Name', required=True)
     code = fields.Char(string='Code', required=True, copy=True, readonly=True, default=lambda self: self.env['ir.sequence'].next_by_code('education.major') or 'New')
-    teacher_id = fields.Many2one('education.teacher', string='Instructor', required=True, help="Main instructor for this course")
-    teacher_ids = fields.Many2many('education.teacher', string='Teachers', help='List teachers for this major')
+    teacher_id = fields.Many2one('res.partner', string='Instructor', required=True, help="Main instructor for this course", domain="[('is_teacher', '=', True)]")
+    teacher_ids = fields.Many2many('res.partner', string='Teachers', help='List teachers for this major', domain="[('is_teacher', '=', True)]")
     curriculum_ids = fields.One2many('education.major.curriculum', 'major_id', string='Curriculum', help='Curriculum Details')
     academic_year = fields.Char(string='Academic Year', help='Academic year for this major')
     color = fields.Integer(string='Color', readonly=True)
