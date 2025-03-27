8354c757d72a4f0004fa4841fa043b9597df7a8c
ridho777 <ridho.artsys@gmail.com>
2025-03-27 17:56:57 +0700

    feat(res_partner): add create user to res.users from res.partner for is_teacher = True.

--- custom_addons/education/education_management/__manifest__.py
+++ custom_addons/education/education_management/__manifest__.py
@@ -31,7 +31,6 @@
         'security/ir.model.access.csv',
         'views/student_attendance_wizard_views.xml',
         'views/education_student_views.xml',
-        # 'views/education_teacher_attendance_views.xml',
         'views/education_classroom_views.xml',
         'views/education_major_views.xml',
         'views/res_partner_views.xml',
--- custom_addons/education/education_management/models/res_partner.py
+++ custom_addons/education/education_management/models/res_partner.py
@@ -1,5 +1,6 @@
-from odoo import models, fields, api
-from datetime import date, timedelta
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+import random, string
 
 class ResPartner(models.Model):
     _inherit ='res.partner'
@@ -17,3 +18,47 @@ class ResPartner(models.Model):
     
     # Administration
     nip = fields.Char(string='NIP')
+    
+    # Teacher Authentication
+    teacher_login_id = fields.Many2one('res.users', string='Related User', readonly=True)
+    
+    def action_create_teacher_user(self):
+        """Membuat user Odoo dari partner jika is_teacher = True"""
+        self.ensure_one()
+        for partner in self:
+            if not partner.teacher_login_id and partner.is_teacher:
+                if not self.email:
+                    raise ValidationError(_("Teacher must have an email to create a user."))
+                
+                # Generate password random
+                password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+                
+                user_vals = {
+                    'name': partner.name,
+                    'login': partner.email,
+                    'partner_id': partner.id,
+                    'groups_id': [(6, 0, [self.env.ref('base.group_user').id])],
+                    'password': password,
+                }
+                
+                new_user = self.env['res.users'].create(user_vals)
+                partner.teacher_login_id = new_user.id
+                
+                print('==============================================')
+                print('Hello World')
+                print('==============================================')
+                
+                # Opsional: Menampilkan password ke user (bisa disimpan di log atau dikirim ke email)
+                message_id = self.env['message.wizard'].create({'message': _(f'User berhasil dibuat!\nLogin: {new_user.login}\nPassword: {password}')})
+                
+                return {
+                    'name': _('Successfull Creating User'),
+                    'type': 'ir.actions.act_window',
+                    'view_mode': 'form',
+                    'res_model': 'message.wizard',
+                    # pass the id
+                    'res_id': message_id.id,
+                    'target': 'new'
+                }
+            else:
+                raise ValidationError(_("User login for this id is already exist."))
