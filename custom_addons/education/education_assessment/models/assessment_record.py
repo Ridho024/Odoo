a2625cc761c59eaf95ba00473f821451634ec70a
ridho777 <ridho.artsys@gmail.com>
2025-04-04 20:38:51 +0700

    feat(assessment_record): update all.

--- custom_addons/education/education_assessment/models/assessment_record.py
+++ custom_addons/education/education_assessment/models/assessment_record.py
@@ -4,20 +4,20 @@ class AssessmentRecord(models.Model):
     _name = 'assessment.record'
     _description = 'Student Assessment Record'
     _rec_name = 'subject_id'
+    _order = 'date asc'
     
-    subject_id = fields.Many2one('education.subject', string='Subject')
+    subject_id = fields.Many2one('education.subject', string='Subject', required=True, help='Subject of the assessment.')
     category = fields.Selection([('daily_task', 'Daily Task'),
                                    ('daily_exam', 'Daily Exam'),
                                    ('midterm_exam', 'Midterm Exam'),
                                    ('final_exam', 'Final Exam'),
-                                   ('practical_exam', 'Practical Exam')], string='Category')
+                                   ('practical_exam', 'Practical Exam')], string='Category', required=True, help='Category for the assessment.')
     semester = fields.Selection([('1', 'Semester 1'),
-                                 ('2', 'Semester 2')], string='Semester')
-    classroom_id = fields.Many2one('education.classroom', string='Classroom')
+                                 ('2', 'Semester 2')], string='Semester', required=True, help='Assessment semester 1 or 2')
+    classroom_id = fields.Many2one('education.classroom', string='Classroom', required=True, help='Classroom for the assessment')
     teacher_id = fields.Many2one('res.partner', string='Teacher', help='Teacher who give the assessment or supervisor')
-    date = fields.Date(string='Date', default= fields.Date.today())
-    assessment_file = fields.Binary(string='Assessment File')
-    total_question = fields.Integer(string='Total Question')
+    date = fields.Date(string='Date', default= fields.Date.today(), required=True)
+    assessment_file = fields.Binary(string='Assessment File', required=True, help='Assessment file in PDF format')
+    total_question = fields.Integer(string='Total Question', required=True)
     assessment_criteria_ids = fields.One2many('assessment.criteria', 'assessment_id', string='Assessment Criteria')
     assessment_result_ids = fields.One2many('assessment.result', 'assessment_id', string='Assessment Results')
-    # student_ids = fields.Many2many('education.student', string='Students', related='classroom_id.student_ids', readonly=True, help='List of student in the classroom choosed')
--- custom_addons/education/education_assessment/models/assessment_result.py
+++ custom_addons/education/education_assessment/models/assessment_result.py
@@ -5,13 +5,10 @@ class AssessmentResult(models.Model):
     _description = 'Student Assessment Result'
     
     assessment_id = fields.Many2one('assessment.record', string='Assessment Record', readonly=True)
-    # total_question = fields.Integer(string='Total Question', related='assessment_id.total_question', readonly=True)
     student_id = fields.Many2one('education.student', string='Student', ondelete='cascade')
     correct_answer = fields.Integer(string='Correct Answer', required=True)
-    incorrect_answer = fields.Integer(string='Incorrect Answer', required=True)
     score = fields.Float(string='Total Score (%)', compute='_calculate_total_score')
-    # student_ids = fields.Many2many('education.student', string='Students', compute='_compute_students', store=False)
-    
+        
     # For report
     semester = fields.Selection([('1', 'Semester 1'),
                                  ('2', 'Semester 2')], string='Semester', compute='_compute_assessment_fields', store=True)
@@ -29,7 +26,7 @@ class AssessmentResult(models.Model):
             record.category = record.assessment_id.category if record.assessment_id else False
             record.subject_id = record.assessment_id.subject_id.id if record.assessment_id else False
     
-    @api.depends('correct_answer', 'incorrect_answer')
+    @api.depends('correct_answer')
     def _calculate_total_score(self):
         for result in self:
             total_question = result.assessment_id.total_question
@@ -38,15 +35,3 @@ class AssessmentResult(models.Model):
                 result.score = total_score
             else:
                 result.score = 0.00
-                
-
-    # @api.constrains("score")
-    # def _check_score(self):
-    #     for record in self:
-    #         if record.score < 0 or record.score > 100:
-    #             raise ValidationError("Nilai harus berada di antara 0 dan 100.")
-    
-"""
-LIST TO DO:
-- Create system untuk absensi penilaian siswa dan guru.
-"""
