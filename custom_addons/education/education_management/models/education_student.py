69fbce7c18e7a9698f6b6d80c4255a12a2d933fe
ridho777 <ridho.artsys@gmail.com>
2025-04-04 20:03:55 +0700

    feat(education_student): delete attendance_ids field in model and views.

--- custom_addons/education/education_management/models/education_student.py
+++ custom_addons/education/education_management/models/education_student.py
@@ -40,7 +40,6 @@ class EducationStudent(models.Model):
     ], string='Status', tracking=True, compute='_compute_status', store=True)
     
     achievement_ids = fields.One2many('education.student.achievement', 'student_id', string='Academic Achievement', tracking=True)
-    attendance_ids = fields.One2many('education.student.attendance', 'student_id', string='Attendances')
     
     @api.depends('classroom_id')
     def _compute_status(self):
