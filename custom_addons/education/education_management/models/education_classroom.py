9eed494b2955ef15a2b1f5c3698e140addd50058
ridho777 <ridho.artsys@gmail.com>
2025-04-07 08:33:00 +0700

    feat(education_classroom): add sql constraint and sequence in model.

--- custom_addons/education/education_management/models/education_classroom.py
+++ custom_addons/education/education_management/models/education_classroom.py
@@ -1,8 +1,11 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class EducationClassroom(models.Model):
     _name = 'education.classroom'
     _description = 'Classroom Information'
+    _sql_constraints = [
+        ('name', 'unique(name)', 'Classroom with this name is already exist!'),
+    ]
     
     name = fields.Char(string='Name', required=True)
     code = fields.Char(string='Code', required=True, copy=False, readonly=True, index=True, default='New')
@@ -17,53 +20,62 @@ class EducationClassroom(models.Model):
     
     @api.model
     def create(self, vals):
-        # Ambil data student_ids sebelum create
+        # Set code jika belum ada
+        if vals.get('code', _('New')) == _('New'):
+            vals['code'] = self.env['ir.sequence'].next_by_code('education.classroom') or _('New')
+
+        # Ambil dan proses student_ids sebelum create
         student_operations = vals.get('student_ids', [])
-        res = super().create(vals)
+
+        # Buat record classroom
+        classroom = super().create(vals)
 
         if student_operations:
             student_env = self.env['education.student']
+            linked_student_ids = []
 
-            # Tangani operasi 6 (replace all)
-            op_6 = next((op for op in student_operations if op[0] == 6), None)
-            if op_6:
-                students = student_env.browse(op_6[2])
-                students.write({'classroom_id': res.id})
+            for op in student_operations:
+                if op[0] == 6:
+                    linked_student_ids.extend(op[2])
+                elif op[0] == 4:
+                    linked_student_ids.append(op[1])
 
-            # Tangani operasi 4 (link)
-            op_4_ids = [op[1] for op in student_operations if op[0] == 4]
-            if op_4_ids:
-                students = student_env.browse(op_4_ids)
-                students.write({'classroom_id': res.id})
+            if linked_student_ids:
+                students = student_env.browse(linked_student_ids)
+                students.write({'classroom_id': classroom.id})
 
-        return res
+        return classroom
+
     
     def write(self, vals):
-        """Memastikan classroom_id diperbarui saat student_ids berubah"""
+        """Pastikan classroom_id pada siswa diperbarui jika student_ids berubah."""
         for classroom in self:
-            old_students = classroom.student_ids  # Simpan daftar siswa sebelum perubahan
+            old_students = classroom.student_ids
 
-            res = super(EducationClassroom, classroom).write(vals)
+            res = super().write(vals)
 
             if 'student_ids' in vals:
-                student_operations = vals['student_ids']
+                student_ops = vals['student_ids']
                 student_env = self.env['education.student']
+                new_student_ids = set()
+                removed_student_ids = set()
 
-                added_students = student_env.browse([op[1] for op in student_operations if op[0] == 4])
-                removed_students = student_env.browse([op[1] for op in student_operations if op[0] == 3])
+                for op in student_ops:
+                    if op[0] == 6:  # Replace all
+                        new_ids = set(op[2])
+                        new_student_ids |= new_ids
+                        removed_student_ids |= set(old_students.ids) - new_ids
+                    elif op[0] == 4:  # Add
+                        new_student_ids.add(op[1])
+                    elif op[0] == 3:  # Remove
+                        removed_student_ids.add(op[1])
+                    elif op[0] == 5:  # Unlink all
+                        removed_student_ids |= set(old_students.ids)
 
-                if added_students:
-                    added_students.write({'classroom_id': classroom.id})
-
-                if removed_students:
-                    removed_students.write({'classroom_id': False})
-
-                if any(op[0] == 6 for op in student_operations):
-                    new_students = student_env.browse(student_operations[0][2])
-                    new_students.write({'classroom_id': classroom.id})
-                    (old_students - new_students).write({'classroom_id': False})
-
-                if any(op[0] == 5 for op in student_operations):
-                    old_students.write({'classroom_id': False})
+                if new_student_ids:
+                    student_env.browse(list(new_student_ids)).write({'classroom_id': classroom.id})
+                if removed_student_ids:
+                    student_env.browse(list(removed_student_ids)).write({'classroom_id': False})
 
         return res
+
