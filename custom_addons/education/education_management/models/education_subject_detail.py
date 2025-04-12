69aee23d0c8e3b468a929494f077d7ed6760e786
ridho777 <ridho.artsys@gmail.com>
2025-04-12 19:10:26 +0700

    feat(education_subject_detail): update constraint model dan fieldcurriculum_id xml.

--- custom_addons/education/education_management/models/education_subject_detail.py
+++ custom_addons/education/education_management/models/education_subject_detail.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class EducationSubjectDetail(models.Model):
     _name = 'education.subject.detail'
@@ -8,3 +9,9 @@ class EducationSubjectDetail(models.Model):
     subject_id = fields.Many2one('education.subject', string='Subject', required=True, ondelete='cascade')
     curriculum_id = fields.Many2one('education.major.curriculum', string='Curriculum', required=True)
     studies = fields.Text(string='Lesson / Study Material', required=True)
+
+    @api.constrains('curriculum_id')
+    def _check_curriculum(self):
+        for record in self:
+            if record.curriculum_id and not isinstance(record.curriculum_id.id, int):
+                raise ValidationError("Please select a valid curriculum from the list. Manual input is not allowed.")
